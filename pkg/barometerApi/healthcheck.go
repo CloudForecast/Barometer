bf4d8c45aff0cf615a971c55056806751ec621ff
Francois Lagier <francois.lagier@gmail.com>
2021-09-02 14:20:59 -0700

    Update HealthCheck to include the appVersion

--- pkg/barometerApi/healthcheck.go
+++ pkg/barometerApi/healthcheck.go
@@ -3,19 +3,24 @@ package barometerApi
 import (
 	"fmt"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 	"time"
 )
 
 func NewHealthCheckEvent() BarometerEvent {
+
+	appVersion := viper.GetString("appVersion")
 	return BarometerEvent{
 		EventType: HealthCheck,
 		EventTs:   time.Now().Unix(),
-		Event:     make(map[string]interface{}),
+		Event:     map[string]interface{}{"appVersion": appVersion},
 	}
 }
 
 func (b BarometerApi) SendHealthCheckEvent() error {
 	log.Debug().Msg("Sending health check...")
+
+
 	event := NewHealthCheckEvent()
 	statusCode, err := b.makePostRequest(event)
 	if err != nil {
